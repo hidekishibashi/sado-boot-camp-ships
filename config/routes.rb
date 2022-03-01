786bf730add0144658140676933a1c4b9532a233
ng-thanh <thban2803@gmail.com>
2022-03-01 17:41:59 +0900

    check input field. if null raise a message

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
+  devise_for :users,
+  controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'top#index'
   get 'fares/car-ferry', to: 'fares#car_ferry'
