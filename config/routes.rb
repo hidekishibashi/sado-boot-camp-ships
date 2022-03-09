a2f35045df37a8eedcb02f797677f6b8e80186de
yoshida <yoshida9823@gmail.com>
2022-03-09 17:25:25 +0900

    sample commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :high_scores
   devise_for :users, controllers: {
      registrations: 'users/registrations',
      sessions: 'users/sessions'
@@ -7,9 +8,7 @@ Rails.application.routes.draw do
   get '/', to: 'top#index'
   get 'fares/car-ferry', to: 'fares#car_ferry'
   get 'fares/jetfoil', to: 'fares#jetfoil'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'top#index'
-  get 'fares/car-ferry', to: 'fares#car_ferry'
-  get 'fares/jetfoil', to: 'fares#jetfoil'
   get 'reserve/index', to: 'reserve#index'
+  get 'hello-world', to: 'hello#index'
+  
 end
